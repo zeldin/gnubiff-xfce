87e6fc2e0225295681634769a8188eeacbeba3ae
Nicolas Rougier <nicolasrougier@users.sourceforge.net>
2004-10-18 09:16:11 +0000

    Synchronization (still discovering CVS...)

--- src/ui-preferences.cc
+++ src/ui-preferences.cc
@@ -29,6 +29,7 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 // ========================================================================
 
+#include <cstdio>
 #include <sstream>
 #include "ui-preferences.h"
 #include "ui-properties.h"
