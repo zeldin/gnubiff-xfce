b296d056666685cdbce248eb4f13acb7884a6958
Robert Sowada <sowadart@users.sourceforge.net>
2005-01-13 21:31:22 +0000

    Macro TEMP_FAILURE_RETURN provided for systems that don't have it

--- src/socket.h
+++ src/socket.h
@@ -116,4 +116,18 @@ public:
 #endif
 };
 
+/* TEMP_FAILURE_RETRY seems to be available only on Linux. For systems that
+ * don't have this macro we provide our own version. This code was taken from
+ * file "/usr/include/unistd.h" from Debian package "libc6-dev"
+ * version 2.3.2.ds1-20. */
+#ifndef TEMP_FAILURE_RETRY
+#define TEMP_FAILURE_RETRY(expression) \
+	(__extension__ \
+	 ({ long int __result; \
+		do __result = (long int) (expression); \
+		while (__result == -1L && errno == EINTR); \
+		__result; }))
+#endif
+
+
 #endif
