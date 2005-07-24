1a7fc7120950e27236318ddd0dc611bf14936286
Robert Sowada <sowadart@users.sourceforge.net>
2005-07-24 20:52:18 +0000

    Workaround: Commented line that causes problems on certain systems

--- src/ui-popup.cc
+++ src/ui-popup.cc
@@ -382,7 +382,7 @@ Popup::show (std::string name)
 	// automatically when presenting the window but may then gained manually
 	gtk_window_set_accept_focus (dialog, false);
 	gtk_window_present (dialog);
-	gtk_window_set_accept_focus (dialog, true);
+//	gtk_window_set_accept_focus (dialog, true);
 
 	if (biff_->value_bool ("popup_use_geometry"))
 		gtk_window_parse_geometry (dialog, biff_->value_gchar ("popup_geometry"));
