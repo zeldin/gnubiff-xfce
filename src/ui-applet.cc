cc13eb2b914e643b34d378b0c3469e06008f8c81
Robert Sowada <sowadart@users.sourceforge.net>
2009-04-05 16:07:18 +0000

    Cleanup:  * Fix compiler warning because of ignoring result of "system" call

--- src/ui-applet.cc
+++ src/ui-applet.cc
@@ -1,6 +1,6 @@
 // ========================================================================
 // gnubiff -- a mail notification program
-// Copyright (c) 2000-2007 Nicolas Rougier, 2004-2007 Robert Sowada
+// Copyright (c) 2000-2009 Nicolas Rougier, 2004-2009 Robert Sowada
 //
 // This program is free software: you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
@@ -142,7 +142,9 @@ Applet::execute_command (std::string option_command,
 	std::string command = biff_->value_string (option_command);
     if (!command.empty ()) {
 		command += " &";
-		system (command.c_str ());
+		int result = system (command.c_str ());
+		if (result == -1)
+			g_warning (_("Cannot execute command \"%s\"."), command.c_str());
 	}
 }
 
